17a7860657c7c020cc47929204da3ba8ec896260
ajbarryiii <ajbarryiii@gmail.com>
2024-12-07 10:08:56 -0800

    p1 passes testcases but fails full. even after fixing int overflow issues

--- d7/d7.cpp
+++ d7/d7.cpp
@@ -1,14 +1,15 @@
 #include <iostream> 
 #include <fstream> 
 #include <sstream>
+#include <stdexcept>
 #include <string> 
 #include <vector> 
 #include <deque>
 
 using namespace std;
 
-vector<pair<int, vector<int>>> parse_file (string filename) {
-	vector<pair<int, vector<int>>> map;
+vector<pair<long, vector<long>>> parse_file (string filename) {
+	vector<pair<long, vector<long>>> map;
 	ifstream file(filename);
 
 	if (!file.is_open()) {
@@ -18,40 +19,45 @@ vector<pair<int, vector<int>>> parse_file (string filename) {
 	string line;
 	while (getline(file, line)) {
 		size_t split = line.find(':');
-		vector<int> line_nums;
+		vector<long> line_nums;
 		string post = line.substr(split+1);
 		istringstream iss(post);
-		int value;
+		long value;
 		while (iss >> value) {
 			line_nums.push_back(value);
 			if ( value < 0) {
 				cout << "value < 0 found \n";
 			}
 		}
-		map.push_back(pair<int,vector<int>>(stoi(line.substr(0, split)), line_nums)); 
+		long tgt;
+		try {
+			tgt = stol(line.substr(0, split));
+		}
+		catch (const out_of_range& e) {
+			cerr << "error: numer out of range in line: " << line << '\n';
+			continue;
+		}
+		map.push_back(pair<long,vector<long>>(tgt, line_nums)); 
 	}
 
 	file.close();
 	return map;
 }
 
-bool dfs( const int &target , const vector<int> &nums) {
-	deque<int> q;
+bool dfs( const long &target , const vector<long> &nums) {
+	deque<long> q;
 	q.push_back(nums[0]);
-	cout << " target : " << target << '\n';
 	for ( int i = 1; i<nums.size(); ++i) {
 		if (!q.empty()){
 			const int q_len = q.size();
-			for (int j = 0; j < q.size(); ++j) {
+			for (int j = 0; j < q_len; ++j) {
 				int curr = q.front();
 				q.pop_front();
-				if ( curr * nums[i] <= target) {
+				if ( (curr * nums[i]) <= target) {
 					q.push_back(curr*nums[i]);
-					cout << curr * nums[i]<< '\n';
 				}
-				if (curr + nums[i] <= target) {
+				if ((curr + nums[i]) <= target) {
 					q.push_back(curr+nums[i]);
-					cout << curr * nums[i]<< '\n';
 				}
 			}
 		}
@@ -59,15 +65,20 @@ bool dfs( const int &target , const vector<int> &nums) {
 			return false;
 		}
 	}
+	for (auto i : q) {
+		if (i == target) return true;
+	}
 	return false;
 }
 
 
-int get_result( const vector<pair<int,vector<int>>> map ) {
-	int result = 0;
+long get_result( const vector<pair<long,vector<long>>> map ) {
+	long result = 0;
 	for( auto line : map ) {
 		bool found = dfs(line.first , line.second);
-		if (found) ++result;
+		if (found){
+			result += line.first;
+		}
 	}
 	return result;
 }
@@ -78,10 +89,9 @@ int main() {
 	cout << "Enter the filename: ";
 	cin >> filename;
 	filename +=".txt";
-	vector<pair<int, vector<int>>> lines = parse_file(filename);
-	//int result = get_result(lines);
-	bool test = dfs(lines.back().first, lines.back().second);
-	//cout << "Result 1: " << result << '\n';
+	vector<pair<long, vector<long>>> lines = parse_file(filename);
+	long result = get_result(lines);
+	cout << "Result 1: " << result << '\n';
 	//cout << "Result 2: " << result2 << '\n';
 	return 0;
 }
