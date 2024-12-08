b71a929401e1aaa56c1b772a85d61e1c49224954
ajbarryiii <ajbarryiii@gmail.com>
2024-12-08 14:09:33 -0800

    finish d7 in ocaml

--- d7/d7o/_build/default/bin/main.ml
+++ d7/d7o/_build/default/bin/main.ml
@@ -12,59 +12,94 @@ let read_lines filename =
 
 open Base
 
+let process_q (curr: int64) (q: int64 list) (tgt:int64) : int64 list = 
+        let rec aux (queue: int64 list) (acc: int64 list) : int64 list = 
+                match queue with
+                | hd:: tl -> (
+                        let sum_check = Int64.(hd + curr <= tgt) in
+                        let mul_check = Int64.(hd * curr <= tgt) in
+                        match (sum_check, mul_check) with
+                        | (true,true) -> aux tl (Int64.(hd * curr)::Int64.(hd + curr):: acc)
+                        | (true,false)-> aux tl (Int64.(hd + curr)::acc)
+                        | (false, true) -> aux tl (Int64.(hd * curr)::acc)
+                        | (false, false) -> aux tl acc
+                )
+                | [] -> acc
+        in 
+        aux q []
+
+let process_q_p2 (curr: int64) (q: int64 list) (tgt:int64) : int64 list = 
+        let rec aux (queue: int64 list) (acc: int64 list) : int64 list = 
+                match queue with
+                | hd:: tl -> (
+                        let sum_check = Int64.(hd + curr <= tgt) in
+                        let mul_check = Int64.(hd * curr <= tgt) in
+                        let concat_check = Int64.(Int64.of_string ((Int64.to_string hd) ^ (Int64.to_string curr)) <= tgt) in
+                        match (sum_check, mul_check,concat_check) with
+                        | true,true,false -> aux tl (Int64.(hd * curr)::Int64.(hd + curr):: acc)
+                        | true,true,true -> aux tl (Int64.of_string ((Int64.to_string hd) ^ (Int64.to_string curr))::Int64.(hd * curr)::Int64.(hd + curr):: acc)
+                        | true,false,false-> aux tl (Int64.(hd + curr)::acc)
+                        | true,false,true-> aux tl (Int64.of_string ((Int64.to_string hd) ^ (Int64.to_string curr))::Int64.(hd + curr)::acc)
+                        | false, true, false -> aux tl (Int64.(hd * curr)::acc)
+                        | false, true, true -> aux tl (Int64.of_string ((Int64.to_string hd) ^ (Int64.to_string curr))::Int64.(hd * curr)::acc)
+                        | false, false,false -> aux tl acc
+                        | false, false,true -> aux tl (Int64.of_string ((Int64.to_string hd) ^ (Int64.to_string curr))::acc)
+                )
+                | [] -> acc
+        in 
+        aux q []
+
+
 let process_line (line: int64 list) : int64 = 
     match line with
-    | hd:: tl -> (
-        let rec level rest q_last tgt acc = 
-            match q_last, acc with 
-            | [],[] -> Int64.zero
-            | _ -> (
-                match rest with 
-                | curr :: [] -> (
-                    match q_last with
-                    | [] -> Int64.zero
-                    | q_acc :: q_rest -> (
-                        let sum_check = Int64.(q_acc + curr <= tgt) in
-                        let mul_check = Int64.(q_acc * curr <= tgt) in
-                        match (sum_check, mul_check) with
-                        | true, _ -> tgt
-                        | _, true -> tgt
-                        | _ -> level q_rest [] tgt []
-                    )
-                )
-                | curr :: back -> (
-                    match q_last with 
-                    | [] -> level back acc tgt []
-                    | q_acc :: q_rest -> (
-                        let sum_check = Int64.(q_acc + curr <= tgt) in
-                        let mul_check = Int64.(q_acc * curr <= tgt) in
-                        match (sum_check, mul_check) with
-                        | (true,true) -> level back q_rest tgt (Int64.(q_acc * curr)::Int64.(q_acc + curr):: acc)
-                        | (true,false)-> level back q_rest tgt (Int64.(q_acc + curr)::acc)
-                        | (false, true) -> level back q_rest tgt (Int64.(q_acc * curr)::acc)
-                        | (false, false) -> level back q_rest tgt acc
-                    )
-                )
-                | _ -> assert false
-            )
+    | hd:: tl -> ( 
+        let level (rest: int64 list) (q_init: int64 list) (tgt: int64) : int64 = 
+                let rec aux (rem: int64 list) (queue: int64 list): int64 list =
+                    match rem with
+                    | curr::[] -> process_q curr queue hd
+                    | curr :: remaining -> aux remaining (process_q curr queue tgt)
+                    | _ -> assert false
+                in
+                let contains = List.mem (aux rest q_init) tgt ~equal: Int64.equal in
+                match contains with
+                |true -> tgt
+                |false -> Int64.zero
         in
         match tl with 
-        | front :: back -> level back [front] hd []
+        | front :: back -> level back [front] hd 
         | _ -> assert false
     )
     | _ -> assert false
 
+let process_line_p2 (line: int64 list) : int64 = 
+    match line with
+    | hd:: tl -> ( 
+        let level (rest: int64 list) (q_init: int64 list) (tgt: int64) : int64 = 
+                let rec aux (rem: int64 list) (queue: int64 list): int64 list =
+                    match rem with
+                    | curr::[] -> process_q_p2 curr queue hd
+                    | curr :: remaining -> aux remaining (process_q_p2 curr queue tgt)
+                    | _ -> assert false
+                in
+                let contains = List.mem (aux rest q_init) tgt ~equal: Int64.equal in
+                match contains with
+                |true -> tgt
+                |false -> Int64.zero
+        in
+        match tl with 
+        | front :: back -> level back [front] hd 
+        | _ -> assert false
+    )
+    | _ -> assert false
 let () = 
         let filename = In_channel.input_line_exn In_channel.stdin in 
         let lines_list = read_lines filename in
         let result1: int64 = List.fold lines_list ~init:Int64.zero ~f:(fun acc line -> 
                 Int64.( acc + (process_line line)) )
                 in
-        (*let result2 = List.fold lines_list ~init:0 ~f:(fun acc line -> 
-                let line = List.map (String.split_on_chars  line ~on:[' ']) ~f:(fun x -> Int.of_string x) in
-                acc + parsed_line_dampener line )
-                in*)
-        printf "Result p1: %Ld \n" result1 
-
+        let result2: int64 = List.fold lines_list ~init:Int64.zero ~f:(fun acc line -> 
+                Int64.( acc + (process_line_p2 line)) )
+                in
+        printf "Result p1: %Ld \n Result p2: %Ld \n" result1 result2
 
 
--- d7/d7o/bin/main.ml
+++ d7/d7o/bin/main.ml
@@ -28,6 +28,27 @@ let process_q (curr: int64) (q: int64 list) (tgt:int64) : int64 list =
         in 
         aux q []
 
+let process_q_p2 (curr: int64) (q: int64 list) (tgt:int64) : int64 list = 
+        let rec aux (queue: int64 list) (acc: int64 list) : int64 list = 
+                match queue with
+                | hd:: tl -> (
+                        let sum_check = Int64.(hd + curr <= tgt) in
+                        let mul_check = Int64.(hd * curr <= tgt) in
+                        let concat_check = Int64.(Int64.of_string ((Int64.to_string hd) ^ (Int64.to_string curr)) <= tgt) in
+                        match (sum_check, mul_check,concat_check) with
+                        | true,true,false -> aux tl (Int64.(hd * curr)::Int64.(hd + curr):: acc)
+                        | true,true,true -> aux tl (Int64.of_string ((Int64.to_string hd) ^ (Int64.to_string curr))::Int64.(hd * curr)::Int64.(hd + curr):: acc)
+                        | true,false,false-> aux tl (Int64.(hd + curr)::acc)
+                        | true,false,true-> aux tl (Int64.of_string ((Int64.to_string hd) ^ (Int64.to_string curr))::Int64.(hd + curr)::acc)
+                        | false, true, false -> aux tl (Int64.(hd * curr)::acc)
+                        | false, true, true -> aux tl (Int64.of_string ((Int64.to_string hd) ^ (Int64.to_string curr))::Int64.(hd * curr)::acc)
+                        | false, false,false -> aux tl acc
+                        | false, false,true -> aux tl (Int64.of_string ((Int64.to_string hd) ^ (Int64.to_string curr))::acc)
+                )
+                | [] -> acc
+        in 
+        aux q []
+
 
 let process_line (line: int64 list) : int64 = 
     match line with
@@ -39,25 +60,46 @@ let process_line (line: int64 list) : int64 =
                     | curr :: remaining -> aux remaining (process_q curr queue tgt)
                     | _ -> assert false
                 in
-                List.mem (aux rest q_init tgt) tgt ~equal: Int64.equal
+                let contains = List.mem (aux rest q_init) tgt ~equal: Int64.equal in
+                match contains with
+                |true -> tgt
+                |false -> Int64.zero
         in
         match tl with 
-        | front :: back -> level back [front] hd []
+        | front :: back -> level back [front] hd 
         | _ -> assert false
     )
     | _ -> assert false
 
+let process_line_p2 (line: int64 list) : int64 = 
+    match line with
+    | hd:: tl -> ( 
+        let level (rest: int64 list) (q_init: int64 list) (tgt: int64) : int64 = 
+                let rec aux (rem: int64 list) (queue: int64 list): int64 list =
+                    match rem with
+                    | curr::[] -> process_q_p2 curr queue hd
+                    | curr :: remaining -> aux remaining (process_q_p2 curr queue tgt)
+                    | _ -> assert false
+                in
+                let contains = List.mem (aux rest q_init) tgt ~equal: Int64.equal in
+                match contains with
+                |true -> tgt
+                |false -> Int64.zero
+        in
+        match tl with 
+        | front :: back -> level back [front] hd 
+        | _ -> assert false
+    )
+    | _ -> assert false
 let () = 
         let filename = In_channel.input_line_exn In_channel.stdin in 
         let lines_list = read_lines filename in
         let result1: int64 = List.fold lines_list ~init:Int64.zero ~f:(fun acc line -> 
                 Int64.( acc + (process_line line)) )
                 in
-        (*let result2 = List.fold lines_list ~init:0 ~f:(fun acc line -> 
-                let line = List.map (String.split_on_chars  line ~on:[' ']) ~f:(fun x -> Int.of_string x) in
-                acc + parsed_line_dampener line )
-                in*)
-        printf "Result p1: %Ld \n" result1 
-
+        let result2: int64 = List.fold lines_list ~init:Int64.zero ~f:(fun acc line -> 
+                Int64.( acc + (process_line_p2 line)) )
+                in
+        printf "Result p1: %Ld \n Result p2: %Ld \n" result1 result2
 
 
