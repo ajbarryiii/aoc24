da4219e8117a6558db73da3467c138d97ece5731
ajbarryiii <ajbarryiii@gmail.com>
2024-12-16 14:03:25 -0800

    starting with approach using dijkstra's algo

--- d16/d16.cpp
+++ d16/d16.cpp
@@ -16,16 +16,16 @@ struct pair_hash {
 };
 
 struct Compare {
-	bool operator()(const pair<int,pair<int,int>>& a, const pair<int,pair<int,int>>& b) {
+	bool operator()(const pair<int,pair<pair<int,int>,vector<int>>>& a, const pair<int,pair<pair<int,int>,vector<int>>>& b) {
 		return a.first>b.first;
 	}
 };
 
-pair<pair<vector<vector<char>>,unordered_map<pair<int,int>,int, pair_hash>>, pair<pair<int,int>,pair<int,int>>> parse_file ( string filename ){
+pair<pair<vector<vector<char>>,unordered_map<pair<int,int>,pair<int,vector<int>>, pair_hash>>, pair<pair<int,int>,pair<int,int>>> parse_file ( string filename ){
 	vector<vector<char>> map;
 	pair<int,int> start_pos;
 	pair<int,int> end_pos;
-	unordered_map<pair<int,int>,int, pair_hash> distances;
+	unordered_map<pair<int,int>,pair<int,vector<int>>, pair_hash> distances;
 	string line;
 	ifstream file(filename);
 	int line_no = 0; 
@@ -47,7 +47,7 @@ pair<pair<vector<vector<char>>,unordered_map<pair<int,int>,int, pair_hash>>, pai
 				end_pos = {line_no,i};
 			}
 			if (line[i] != '#'){
-				distances[{line_no,i}] = INT_MAX;
+				distances[{line_no,i}] = {INT_MAX,{0,0}};
 			}
 		}
 		map.push_back(curr_line);
@@ -57,15 +57,58 @@ pair<pair<vector<vector<char>>,unordered_map<pair<int,int>,int, pair_hash>>, pai
 	return {{map,distances}, {start_pos,end_pos}};
 }
 
-int maze_score (pair<pair<vector<vector<char>>,unordered_map<pair<int,int>,int, pair_hash>>, pair<pair<int,int>,pair<int,int>>> input){
+int maze_score (pair<pair<vector<vector<char>>,unordered_map<pair<int,int>,pair<int,vector<int>>, pair_hash>>, pair<pair<int,int>,pair<int,int>>> input){
 	const vector<vector<char>> map = input.first.first;
-	unordered_map<pair<int,int>,int, pair_hash> distances = input.first.second;
+	// point: {distance, dir} -> need the dir so that we can find cost of next move
+	unordered_map<pair<int,int>,pair<int,vector<int>>, pair_hash> distances = input.first.second;
 	unordered_set<pair<int,int>, pair_hash> visited;
 	pair<int,int> start_pos = input.second.first;
 	pair<int,int> end_pos = input.second.second;
-	priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>, Compare> heap;
-	heap.push({0,start_pos});
-	vector<int> dir = {-1,0};
-
+	// do we need the dir in the heap?
+	priority_queue<pair<int,pair<pair<int,int>,vector<int>>>,vector<pair<int,pair<pair<int,int>,vector<int>>>>, Compare> heap;
+	heap.push({0,{start_pos,{-1,0}}});
+	distances[start_pos] = {0,{-1,0}};
 	// use rotation matrices for computation of next;
+	while (!heap.empty()) { 
+		pair<int,pair<pair<int,int>,vector<int>>> curr = heap.top();
+		int dist = curr.first;
+		vector<int> dir = curr.second.second;
+		pair<int,int> pos = curr.second.first;
+		
+		if (!visited.contains(pos)) {
+			visited.insert(pos);
+		}
+		// 90 deg rotations;
+		vector<int> left90 = vector<int> {-1*dir[1], dir[0]};
+		vector<int> right90 = vector<int> {dir[1], -1*dir[0]}; 
+		if (map[pos.first+dir[0]][pos.second+dir[1]] != '#'){
+			if ( distances[{pos.first+dir[0],pos.second+dir[1]}].first > dist+1) { // should this line be >=?
+				distances[{pos.first+dir[0],pos.second+dir[1]}] = {dist+1, dir};
+				heap.push({dist+1, {{pos.first+dir[0],pos.second+dir[1]}, dir}});
+			}
+		}
+		if (map[pos.first+left90[0]][pos.second+left90[1]] != '#'){
+			if ( distances[{pos.first+left90[0],pos.second+left90[1]}].first > dist+1001) { // should this line be >=?
+				distances[{pos.first+left90[0],pos.second+left90[1]}] = {dist+1001, left90};
+				heap.push({dist+1001, {{pos.first+left90[0],pos.second+left90[1]}, left90}});
+			}
+		}
+		if (map[pos.first+right90[0]][pos.second+right90[1]] != '#'){
+			if ( distances[{pos.first+right90[0],pos.second+right90[1]}].first > dist+1001) { // should this line be >=?
+				distances[{pos.first+right90[0],pos.second+right90[1]}] = {dist+1001, right90};
+				heap.push({dist+1, {{pos.first+right90[0],pos.second+right90[1]}, right90}});
+			}
+		}
+	}
+	return distances[end_pos].first;
+}
+
+int main() {
+	string filename;
+	cout << "input the filename:";
+	cin >> filename;
+	filename += ".txt";
+	pair<pair<vector<vector<char>>,unordered_map<pair<int,int>,pair<int,vector<int>>, pair_hash>>, pair<pair<int,int>,pair<int,int>>> input = parse_file(filename);
+	int score1 = maze_score(input);
+	cout << "result 1: " << score1 << '\n';
 }
