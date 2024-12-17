abd00c83b2d9faf4f3ec725a23d476d688be3f68
ajbarryiii <ajbarryiii@gmail.com>
2024-12-16 22:43:36 -0800

    complete p2

--- d16/d16.cpp
+++ d16/d16.cpp
@@ -103,12 +103,102 @@ int maze_score (pair<pair<vector<vector<char>>,unordered_map<pair<int,int>,pair<
 	return distances[end_pos].first;
 }
 
+int maze_score_p2 (pair<pair<vector<vector<char>>,unordered_map<pair<int,int>,pair<int,vector<int>>, pair_hash>>, pair<pair<int,int>,pair<int,int>>> input){
+	const vector<vector<char>> map = input.first.first;
+	// point: {distance, dir} -> need the dir so that we can find cost of next move
+	unordered_map<pair<int,int>,pair<int,vector<int>>, pair_hash> distances = input.first.second;
+	pair<int,int> start_pos = input.second.first;
+	pair<int,int> end_pos = input.second.second;
+	// do we need the dir in the heap?
+	priority_queue<pair<int,pair<pair<int,int>,vector<int>>>,vector<pair<int,pair<pair<int,int>,vector<int>>>>, Compare> heap;
+	heap.push({0,{start_pos,{0,1}}});
+	distances[start_pos] = {0,{0,1}};
+	// use rotation matrices for computation of next;
+	while (!heap.empty()) { 
+		pair<int,pair<pair<int,int>,vector<int>>> curr = heap.top();
+		int dist = curr.first;
+		vector<int> dir = curr.second.second;
+		pair<int,int> pos = curr.second.first;
+		heap.pop();
+		// 90 deg rotations;
+		vector<int> left90 = vector<int> {-1*dir[1], dir[0]};
+		vector<int> right90 = vector<int> {dir[1], -1*dir[0]}; 
+		if (map[pos.first+dir[0]][pos.second+dir[1]] != '#'){
+			if ( distances[{pos.first+dir[0],pos.second+dir[1]}].first > dist+1) { // should this line be >=?
+				distances[{pos.first+dir[0],pos.second+dir[1]}] = {dist+1, dir};
+				heap.push({dist+1, {{pos.first+dir[0],pos.second+dir[1]}, dir}});
+			}
+		}
+		if (map[pos.first+left90[0]][pos.second+left90[1]] != '#'){
+			if ( distances[{pos.first+left90[0],pos.second+left90[1]}].first > dist+1001) { // should this line be >=?
+				distances[{pos.first+left90[0],pos.second+left90[1]}] = {dist+1001, left90};
+				heap.push({dist+1001, {{pos.first+left90[0],pos.second+left90[1]}, left90}});
+			}
+		}
+		if (map[pos.first+right90[0]][pos.second+right90[1]] != '#'){
+			if ( distances[{pos.first+right90[0],pos.second+right90[1]}].first > dist+1001) { // should this line be >=?
+				distances[{pos.first+right90[0],pos.second+right90[1]}] = {dist+1001, right90};
+				heap.push({dist+1001, {{pos.first+right90[0],pos.second+right90[1]}, right90}});
+			}
+		}
+	}
+	// is this a problem with the way I calculated the distances?;
+	vector<vector<int>> directions = {{-1,0}, {1,0},{0,-1}, {0,1}};
+	unordered_set<pair<int,int>, pair_hash> optimal_path;
+	vector<pair<int,int>> q;
+	optimal_path.insert(end_pos);
+	q.push_back(end_pos);
+	// code is not properly handling the case where we reach a turning (branch) point. what if we add a direction component, check 
+	while(!q.empty()){
+		pair<int,int> curr = q.back();
+		int curr_dist = distances[curr].first;
+		//cout << "current: (" << curr.first << ',' << curr.second <<"), curr_dist = " << curr_dist << '\n';
+		q.pop_back();
+		for (int i = 0; i < directions.size(); ++i){
+			pair<int,int> next = {curr.first+directions[i][0], curr.second+directions[i][1]};
+			if (distances.contains(next)) {
+				//cout << "next: (" << next.first << ',' << next.second <<"), dist = " << distances[next].first << ", direction (og): " << distances[next].second[0] << ',' << distances[next].second[1]<< '\n';
+				if (distances[next].first < curr_dist) {
+					q.push_back(next);
+					optimal_path.insert(next);
+				}
+				if (distances[curr].second != distances[next].second ) {
+					//cout << "case you are working on\n";
+					if (distances.contains({next.first-distances[curr].second[0],next.second-distances[curr].second[1]})) {
+						if (distances[{next.first-distances[curr].second[0],next.second-distances[curr].second[1]}].first == curr_dist-2) {
+							q.push_back({next.first-distances[curr].second[0],next.second-distances[curr].second[1]});
+							optimal_path.insert({next.first-distances[curr].second[0],next.second-distances[curr].second[1]});
+						}
+					}
+				}
+			}
+		}
+		//cout <<'\n';
+	}
+	//for (int i = 0; i < map.size(); ++i) {
+	//	for (int j = 0; j<map[0].size(); ++j) {
+	//		if (optimal_path.contains({i,j})){
+	//			cout <<'O';
+	//		}
+	//		else {
+	//			cout << map[i][j];
+	//		}
+	//	}
+	//	cout <<' '<< i <<'\n';
+	//}
+	return optimal_path.size();
+}
+
 int main() {
+	
 	string filename;
 	cout << "input the filename:";
 	cin >> filename;
 	filename += ".txt";
 	pair<pair<vector<vector<char>>,unordered_map<pair<int,int>,pair<int,vector<int>>, pair_hash>>, pair<pair<int,int>,pair<int,int>>> input = parse_file(filename);
 	int score1 = maze_score(input);
+	int score2 = maze_score_p2(input);
+	// difficulty of p2 is that there can be multiple best paths through the maze, what happens if we just assume otherwise?
 	cout << "result 1: " << score1 << '\n';
+	cout << "result 2: " << score2 << '\n';
 }
