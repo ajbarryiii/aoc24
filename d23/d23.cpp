59070995a12f69cd69b014c20b13d7326f41a4c8
ajbarryiii <ajbarryiii@gmail.com>
2024-12-30 13:23:51 -0800

    p2 first attempt,VERY slow, passes tests, fails ans

--- d23/d23.cpp
+++ d23/d23.cpp
@@ -4,6 +4,8 @@
 #include <unordered_set>
 #include <cassert>
 #include <fstream> 
+#include <algorithm>
+#include <variant>
 
 using namespace std;
 
@@ -50,6 +52,61 @@ int get_sets_of_3 (unordered_map<string,unordered_set<string>> map ) {
 	return count;
 }
 
+vector<string> find_max_clique (unordered_map<string,unordered_set<string>> map) {
+	vector<unordered_set<string>> curr_cliques;
+	//tactic: build up cliques recursively;
+	//first init the curr_cliques of size 2;
+	unordered_map<string,unordered_set<string>> map_copy = map;
+	for (auto node:map) {
+		for (auto rel:node.second) {
+			unordered_set<string> clique;
+			map_copy[rel].erase(node.first);
+			clique.insert(rel);
+			clique.insert(node.first);
+			curr_cliques.push_back(clique);
+		}
+	}
+	//How do we not double count for the next step?
+	while (true) {
+		vector<unordered_set<string>> next_cliques;
+		unordered_set<string> visited;
+		cout << curr_cliques.size() << '\n';
+		for (int i=0;i<curr_cliques.size();++i) {
+			for (auto key:map) {
+				if (!curr_cliques[i].contains(key.first)) {
+					bool valid = all_of ( curr_cliques[i].begin(), curr_cliques[i].end(), 
+							[&] (string s) { return map[s].contains(key.first);});
+					if (valid) {
+						unordered_set<string> clique_next = curr_cliques[i];
+						clique_next.insert(key.first);
+						vector<string> prevent_double;
+						for(auto item:clique_next) {
+							prevent_double.push_back(item);
+						}
+						sort(prevent_double.begin(),prevent_double.end());
+						string prevent_double_check = "";
+						for (int str=0;str<prevent_double.size();++str) prevent_double_check+=prevent_double[str];
+						if (!visited.contains(prevent_double_check)) {
+							next_cliques.push_back(clique_next);
+							visited.insert(prevent_double_check);
+						}
+					}
+				}
+			}
+		}
+		if (!next_cliques.empty()) {
+			curr_cliques = next_cliques;
+		}
+		else break;
+	}
+	unordered_set<string> max_clique = curr_cliques[0];
+	vector<string> result;
+	for (auto node:max_clique) {
+		result.push_back(node);
+	}
+	return result;
+}
+
 int main () {
 	unordered_map<string,unordered_set<string>> map_test = parse_file("d23t.txt");
 	int count_g3_t = get_sets_of_3(map_test);
@@ -57,5 +114,11 @@ int main () {
 	unordered_map<string,unordered_set<string>> map = parse_file("d23.txt");
 	int result1 = get_sets_of_3(map);
 	cout << "Result 1: " << result1 << '\n';
+	vector<string> testp2 = find_max_clique(map);
+	cout << "testp2: ";
+	for (int i=0;i<testp2.size();++i) {
+		cout << testp2[i] << ',';
+	}
+	cout << '\n';
 	return 0;
 }
